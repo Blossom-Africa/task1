e18bf975dd0b170079d65d0e6d3699b42988eddd
Blossom Africa <courses@blossom.africa>
2026-06-24 01:09:18 +0100

    latest news

---
+++ app.py
@@ -0,0 +1,13 @@
+# greeting.py
+# This file is used for Task 1 — git pull practice
+
+def greet(name):
+    return f"Hello {name}, welcome to Blossom Academy!"
+
+def farewell(name):
+    return f"Goodbye {name}, see you next class!"
+
+
+if __name__ == "__main__":
+    print(greet("Victor"))
+    print(farewell("Victor"))
